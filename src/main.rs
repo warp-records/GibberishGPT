0e33bef0a857fe4246570e073c6352985156d81a
Rift <no@e.mail>
2024-05-27 19:09:11 -0400

    Organize file structure

--- src/main.rs
+++ src/main.rs
@@ -40,13 +40,13 @@ fn main() {
 
     let mut matrix: HashMap<String, HashMap<String, u32>> = HashMap::new();
 
-    let token_length = 3;
+    let token_length = 1;
 
     //Words that exist for sentence structure but don't
     //convey ideas
 
     let stop_words = [
-        "fjkdaf",//"and", "the", "is", "are", "to", "of", "a", "an", "in", "for", "on", "but", "that", "it", "as"
+       "and", "the", "is", "are", "to", "of", "a", "an", "in", "for", "on", "but", "that", "it", "as"
     ];
     //outer key is previous word, inner key is stop word
     let mut stop_phrase_matrix: HashMap<String, HashMap<String, u32>> = HashMap::new();
@@ -57,7 +57,7 @@ fn main() {
         .map(|s| s.to_lowercase()
             .chars()
             .filter(|c| !c.is_ascii_punctuation() && !c.is_whitespace())
-            .collect::<String>());
+            .collect::<String>()).peekable();
 
 
     let mut last_token = String::new();
@@ -70,28 +70,31 @@ fn main() {
     let mut iter = 0;
 
     while let Some(expr) = expressions_itr.next() {
-        /*
+        
         if stop_words.contains(&expr.as_str()) {
             //let mut sequence = expr.clone();
             //Uncommenting will make it keep collecting
             //stop words into a sequence
-            while let Some(next_word) = expressions_itr.next() {
+
+            while let Some(next_word) = expressions_itr.peek() {
+                let mut sequence = String::new();
 
                 if stop_words.contains(&next_word.as_str()) {
-                    sequence = sequence + next_word.as_str();
+                    sequence += next_word.as_str();
+                    expressions_itr.next();
+
                 } else {
                     stop_phrase_matrix
-                        .entry(last_expr.to_string())
+                        .entry(expr.to_string())
                         .or_insert_with(HashMap::new)
                         .entry(sequence.to_string())
                         .and_modify(|count| *count += 1)
                         .or_insert(1);
 
-                    last_expr = expr;
                     break;
                 }
             }
-        } else {*/
+        } else {
             curr_token += expr.as_str();
 
             if iter % token_length == 0 {
@@ -108,7 +111,8 @@ fn main() {
             } else {
                 curr_token += " ";
             }
-        //}
+        }
+
         iter += 1;
     }
 
@@ -117,49 +121,57 @@ fn main() {
 
     let mut rng = rand::thread_rng();
 
-    let mut expr = matrix.keys().nth(rng.gen_range(0..matrix.len()-1)).unwrap().to_string();
+    let mut token = matrix.keys().nth(rng.gen_range(0..matrix.len()-1)).unwrap().to_string();
 
     //Generate text
     for _ in 0..num_phrases {
-        print!("{expr} ");
+        //print!("{expr} ");
 
         let mut max_entry_cnt = 0;
-        let mut next_expr = String::new();
+        let mut next_token = String::new();
 
-        if !matrix.contains_key(&expr) {
-            expr = matrix.keys().nth(rng.gen_range(0..matrix.len()-1)).unwrap().to_string();
+        if !matrix.contains_key(&token) {
+            token = matrix.keys().nth(rng.gen_range(0..matrix.len()-1)).unwrap().to_string();
             continue;
         }
 
-        for (entry_string, count) in &matrix[&expr] {
+        for (entry_string, count) in &matrix[&token] {
             if *count > max_entry_cnt {
                 max_entry_cnt = *count;
-                next_expr = entry_string.to_string();
+                next_token = entry_string.to_string();
             }
         }
 
 
+        let mut token_part_itr = next_token.split_whitespace();
 
-        if stop_phrase_matrix.contains_key(&expr) {
-            //Find most common stop word that occurs
-            //after given expression
-            let mut max_stop_entry_cnt = 0;
-            let mut stop_phrase = String::new();
+        while let Some(expr) = token_part_itr.next() {
+            print!("{expr} ");
 
-            for (entry_string, count) in &stop_phrase_matrix[&expr] {
-                if *count > max_entry_cnt {
-                    max_stop_entry_cnt = *count;
-                    stop_phrase = entry_string.to_string();
+            if stop_phrase_matrix.contains_key(expr) {
+                //Find most common stop word that occurs
+                //after given expression
+                let mut max_stop_entry_cnt = 0;
+                let mut stop_phrase = String::new();
+
+                for (entry_string, count) in &stop_phrase_matrix[expr] {
+                    if *count > max_entry_cnt {
+                        max_stop_entry_cnt = *count;
+                        stop_phrase = entry_string.to_string();
+                    }
+                }
+
+                //More often than not, stop word occurs before next phrase
+                if max_stop_entry_cnt*2 > max_entry_cnt {
+                    print!("{stop_phrase} ");
                 }
             }
-
-            //More often than not, stop word occurs before next phrase
-            if max_stop_entry_cnt*2 > max_entry_cnt {
-                print!("{stop_phrase} ");
-            }
         }
 
-        *matrix.get_mut(&expr).unwrap().get_mut(&next_expr).unwrap() = 0;
-        expr = next_expr;
+        if matrix[&token].contains_key(&next_token) {
+            *matrix.get_mut(&token).unwrap().get_mut(&next_token).unwrap() = 0;
+        }
+        //*matrix.get_mut(&token).unwrap().get_mut(&next_token).unwrap() = 0;
+        token = next_token;
     }
 }
